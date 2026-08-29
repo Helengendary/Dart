d6cf50ae108d396bbff96d550f65181130de6683
Helengendary <helenapicinin13@gmail.com>
2026-08-29 16:28:55 -0300

    exercício adicionado

---
+++ lib/receita_model.dart
@@ -0,0 +1,23 @@
+class Receita {
+  final String id;
+
+  final String titulo;
+
+  final String ingredientes;
+
+  final String modoDePreparo;
+
+  final String imageUrl;
+
+  Receita({
+    required this.id,
+
+    required this.titulo,
+
+    required this.ingredientes,
+
+    required this.modoDePreparo,
+
+    required this.imageUrl,
+  });
+}
